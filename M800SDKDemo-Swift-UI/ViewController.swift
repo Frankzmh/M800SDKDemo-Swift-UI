c7fb5ef20788b4e6e90257f91dab1f9043c4c4d1
THE-ZMH <1415502058@qq.com>
2017-11-23 18:51:53 +0800

    Initial Commit

---
+++ M800SDKDemo-Swift-UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  M800SDKDemo-Swift-UI
+//
+//  Created by 张明红 on 23/11/2017.
+//  Copyright © 2017 张明红. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
